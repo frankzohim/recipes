de0b0909d14e0cc728ef766f29312642f3b58773
Frank Zohim <delanofofe@gmail.com>
2023-08-25 18:10:41 +0100

    commit from monaco

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.components.css'],
+})
+export class RecipesComponent {}
